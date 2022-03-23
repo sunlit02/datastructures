753d0c565ed052e8d696c0d850b480f09cf8bf1c
sunlit02 <98438621+sunlit02@users.noreply.github.com>
2022-03-23 16:29:53 +0900

    Update 3_8.py

--- 3_8.py
+++ 3_8.py
@@ -1,4 +1,6 @@
-# n번째 조화수를 구하는 재귀함수 작 조화수 : Hn = 1 + 1/2 + 1/3 + ... + 1/n
+# n번째 조화수를 구하는 재귀함수 작성 / 조화수 : Hn = 1 + 1/2 + 1/3 + ... + 1/n
+# write a recursive function to find n'th harmonic number
+
 def harmonicnumber(a, s):
     if a == 1:
         return s+1

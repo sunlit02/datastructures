7eb29d4a024985e382116808f071c64d9cb32462
sunlit02 <98438621+sunlit02@users.noreply.github.com>
2022-03-29 12:08:58 +0900

    Update 3_6.py

--- 3_6.py
+++ 3_6.py
@@ -1,5 +1,11 @@
 def perm(word, i, n):
     if i == n:
-        print(word)
+        print(''.join(word), end=' ')
     else:
-    for j in range(i, n+1):
+        for j in range(i, n+1):
+            word[i], word[j] = word[j], word[i]
+            perm(word, i+1, n)
+            word[i], word[j] = word[j], word[i]
+          
+word = ['L','A','N','G']
+perm(word,0,3)
